73e9549c5f392903d5f75cfd63497168609869eb
Sherxon <116145014+SherxonBoboyorov@users.noreply.github.com>
2023-08-27 09:53:07 +0500

    contacts page ended

--- resources/views/front/contacts.blade.php
+++ resources/views/front/contacts.blade.php
@@ -53,12 +53,8 @@
                   <div class="title flex w-fit items-center">
                     <span>
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="20" viewBox="0 0 16 20" fill="none">
-                        <path
-                          d="M8 9C8.82843 9 9.5 8.32843 9.5 7.5C9.5 6.67157 8.82843 6 8 6C7.17157 6 6.5 6.67157 6.5 7.5C6.5 8.32843 7.17157 9 8 9Z"
-                          fill="#FC7E1F" />
-                        <path
-                          d="M8 1.00149e-08C5.89206 -0.00010539 3.86926 0.831759 2.37124 2.31479C0.873231 3.79782 0.0210794 5.81216 0 7.92C0 13.4 7.05 19.5 7.35 19.76C7.53113 19.9149 7.76165 20.0001 8 20.0001C8.23835 20.0001 8.46887 19.9149 8.65 19.76C9 19.5 16 13.4 16 7.92C15.9789 5.81216 15.1268 3.79782 13.6288 2.31479C12.1307 0.831759 10.1079 -0.00010539 8 1.00149e-08ZM8 11C7.30777 11 6.63108 10.7947 6.0555 10.4101C5.47993 10.0256 5.03133 9.47893 4.76642 8.83939C4.50151 8.19985 4.4322 7.49612 4.56725 6.81718C4.7023 6.13825 5.03564 5.51461 5.52513 5.02513C6.01461 4.53564 6.63825 4.2023 7.31718 4.06725C7.99612 3.9322 8.69985 4.00151 9.33939 4.26642C9.97893 4.53133 10.5256 4.97993 10.9101 5.5555C11.2947 6.13108 11.5 6.80777 11.5 7.5C11.5 8.42826 11.1313 9.3185 10.4749 9.97487C9.8185 10.6313 8.92826 11 8 11Z"
-                          fill="#FC7E1F" />
+                        <path d="M8 9C8.82843 9 9.5 8.32843 9.5 7.5C9.5 6.67157 8.82843 6 8 6C7.17157 6 6.5 6.67157 6.5 7.5C6.5 8.32843 7.17157 9 8 9Z" fill="#FC7E1F" />
+                        <path  d="M8 1.00149e-08C5.89206 -0.00010539 3.86926 0.831759 2.37124 2.31479C0.873231 3.79782 0.0210794 5.81216 0 7.92C0 13.4 7.05 19.5 7.35 19.76C7.53113 19.9149 7.76165 20.0001 8 20.0001C8.23835 20.0001 8.46887 19.9149 8.65 19.76C9 19.5 16 13.4 16 7.92C15.9789 5.81216 15.1268 3.79782 13.6288 2.31479C12.1307 0.831759 10.1079 -0.00010539 8 1.00149e-08ZM8 11C7.30777 11 6.63108 10.7947 6.0555 10.4101C5.47993 10.0256 5.03133 9.47893 4.76642 8.83939C4.50151 8.19985 4.4322 7.49612 4.56725 6.81718C4.7023 6.13825 5.03564 5.51461 5.52513 5.02513C6.01461 4.53564 6.63825 4.2023 7.31718 4.06725C7.99612 3.9322 8.69985 4.00151 9.33939 4.26642C9.97893 4.53133 10.5256 4.97993 10.9101 5.5555C11.2947 6.13108 11.5 6.80777 11.5 7.5C11.5 8.42826 11.1313 9.3185 10.4749 9.97487C9.8185 10.6313 8.92826 11 8 11Z" fill="#FC7E1F" />
                       </svg>
                     </span>
                     <span class="ml-2 text-dimgray mt-1 font-[600] md:text-[24px] max-md:text-[21px]">
@@ -69,16 +65,15 @@
                   <div class="text-item text-dimgray md:text-[18px] max-md:text-[16px] my-5">
                     <span>Адрес:</span>
                     <br>
-                    <a href="https://www.google.com/maps/search/+%D0%A3%D0%B7%D0%B1%D0%B5%D0%BA%D0%B8%D1%81%D1%82%D0%B0%D0%BD,+%D0%B3.+%D0%A2%D0%B0%D1%88%D0%BA%D0%B5%D0%BD%D1%82%D0%A3%D0%BB.+%D0%A2%D0%B5%D0%BC%D1%83%D1%80+%D0%9C%D0%B0%D0%BB%D0%B8%D0%BA,+%D0%B4%D0%BE%D0%BC+17%D0%B0/@41.2905012,69.3319186,18z?entry=ttu"
-                      class="font-[600]">
-                      Узбекистан, г. ТашкентУл. Темур Малик, дом 17а
+                    <a class="font-[600]">
+                      {{ $options->where('key', 'address_' . app()->getLocale())->first()->value }}
                     </a>
                   </div>
                   <div class="text-item text-dimgray md:text-[18px] max-md:text-[16px] my-5">
                     <span>Телефонный номер:</span>
                     <br>
-                    <a href="tel:+99 893 505 45 05" class="font-[600]">
-                      +99 893 505 45 05
+                    <a href="tel:{{ $options->where('key', 'phone')->first()->value }}" class="font-[600]">
+                      {{ $options->where('key', 'phone')->first()->value }}
                     </a>
                   </div>
                   <div class="text-item text-dimgray md:text-[18px] max-md:text-[16px] my-5">
@@ -86,17 +81,14 @@
                       Электронная почта:
                     </span>
                     <br>
-                    <a href="mailto:info@sos.uz" class="font-[600]">
-                      info@sos.uz
+                    <a href="mailto:{{ $options->where('key', 'email')->first()->value }}" class="font-[600]">
+                      {{ $options->where('key', 'email')->first()->value }}
                     </a>
                   </div>
                 </div>
                 <div class="right-side sm:w-[50%] max-sm:w-full sm:h-full max-sm:h-[300px]">
-                  <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d11980.718583378304!2d69.34735085!3d41.348448850000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1z0JDQtNGA0LXRgTog0KPQt9Cx0LXQutC40YHRgtCw0L0sINCzLiDQotCw0YjQutC10L3RgtCj0LsuINCi0LXQvNGD0YAg0JzQsNC70LjQuiwg0LTQvtC8IDE30LA!5e0!3m2!1sru!2s!4v1692903609692!5m2!1sru!2s"
-                    class="w-full h-full" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
-                    style="border: 0" allowfullscreen="" loading="lazy"
-                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+                  {!! $options->where('key', 'map')->first()->value !!}
+
                 </div>
               </div>
 
